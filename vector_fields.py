59ef9455f5c1830c6eaa415728b12c2c503ec480
AaronHavens <ahavens2@illinois.edu>
2024-01-13 00:46:25 -0600

    weird spacing

--- vector_fields.py
+++ vector_fields.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 
 
 class simpleVF(nn.Module):
-	def __init__(self, input_dim, hid_dim=128):
-		super(simpleVF, self).__init__()
-		# input includes stacked time dimension
-		self.net = nn.Sequential(	nn.Linear(input_dim+1, hid_dim),
-									nn.ReLU(),
-									nn.Linear(hid_dim, hid_dim),
-									nn.ReLU(),
-									nn.Linear(hid_dim, hid_dim),
-									nn.ReLU(),
-									nn.Linear(hid_dim, input_dim))
+    def __init__(self, input_dim, hid_dim=128):
+        super(simpleVF, self).__init__()
+        # input includes stacked time dimension
+        self.net = nn.Sequential(   nn.Linear(input_dim+1, hid_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(hid_dim, hid_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(hid_dim, hid_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(hid_dim, input_dim))
 
-	def forward(self, x):
-		return self.net(x)
+    def forward(self, x):
+        return self.net(x)
 
 
 
@@ -25,28 +25,28 @@ class simpleVF(nn.Module):
 
 
 def circle_barrier(x):
-	x1 = x[:,0]
+    x1 = x[:,0]
 
-	return -torch.square(x1) + 0.5**2
+    return -torch.square(x1) + 0.5**2
 
 def circle_barrier_cond(x, V):
-	x1 = x[:,0]
-	grad_h_x1 = -2*x1
+    x1 = x[:,0]
+    grad_h_x1 = -2*x1
 
 
-	return grad_h_x1 * V[:,0] + 10.*circle_barrier(x)
+    return grad_h_x1 * V[:,0] + 10.*circle_barrier(x)
 
 def neg_circle_barrier(x):
-	x1 = x[:,0]
-	x2 = x[:,1]
+    x1 = x[:,0]
+    x2 = x[:,1]
 
-	return torch.square(x1) + torch.square(x2-0.5) - 0.25**2
+    return torch.square(x1) + torch.square(x2-0.5) - 0.25**2
 
 def neg_circle_barrier_cond(x, V):
-	x1 = x[:,0]
-	x2 = x[:,1]
+    x1 = x[:,0]
+    x2 = x[:,1]
 
-	grad_h_x1 = 2*x1
-	grad_h_x2 = 2*(x2-0.5)
+    grad_h_x1 = 2*x1
+    grad_h_x2 = 2*(x2-0.5)
 
-	return grad_h_x1 * V[:,0] + grad_h_x2 * V[:,1] + 10. * neg_circle_barrier(x) 
+    return grad_h_x1 * V[:,0] + grad_h_x2 * V[:,1] + 10. * neg_circle_barrier(x) 
